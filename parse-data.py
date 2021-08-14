f540bc76658b3c8ca62a05c839765ccbb09cac4a
guillermina <suttersg@gmail.com>
2021-08-14 19:04:32 -0400

    Data Analysis Update

--- parse-data.py
+++ parse-data.py
@@ -2,6 +2,7 @@ import pandas as pd
 import datetime
 import os
 import matplotlib.pyplot as plt
+import string
 
 cwd = os.getcwd()
 print('cwd:', cwd)
@@ -83,7 +84,104 @@ sumj_df = date_j.groupby(pd.Grouper(key='Date', axis=0,
 sumg_df = date_g.groupby(pd.Grouper(key='Date', axis=0,
                                     freq='D')).sum()
 
-
 # sumj_df.to_csv('Him.csv')
 # sumg_df.to_csv('Her.csv')
+
+# Get all punctuation marks
+februari = []
+mart = []
+april = []
+mai = []
+juni = []
+
+gfebruari = []
+gmart = []
+gapril = []
+gmai = []
+gjuni = []
+
+clean_df_j = clean_df[clean_df['Name'].isin(['Hardonk Jomme'])]
+clean_df_g = clean_df[clean_df['Name'].isin(['Guillermina'])]
+
+for i in range(len(clean_df_j)):
+    if clean_df_j.iloc[i, 0].month == 2:
+        for j in clean_df_j.iloc[i, 2]:
+            if j in string.punctuation:
+                februari.append(j)
+
+    if clean_df_j.iloc[i, 0].month == 3:
+        for j in clean_df_j.iloc[i, 2]:
+            if j in string.punctuation:
+                mart.append(j)
+
+    if clean_df_j.iloc[i, 0].month == 4:
+        for j in clean_df_j.iloc[i, 2]:
+            if j in string.punctuation:
+                april.append(j)
+
+    if clean_df_j.iloc[i, 0].month == 5:
+        for j in clean_df_j.iloc[i, 2]:
+            if j in string.punctuation:
+                mai.append(j)
+
+    if clean_df_j.iloc[i, 0].month == 6:
+        for j in clean_df_j.iloc[i, 2]:
+            if j in string.punctuation:
+                juni.append(j)
+
+for i in range(len(clean_df_g)):
+    if clean_df_g.iloc[i, 0].month == 2:
+        for j in clean_df_g.iloc[i, 2]:
+            if j in string.punctuation:
+                gfebruari.append(j)
+
+    if clean_df_g.iloc[i, 0].month == 3:
+        for j in clean_df_g.iloc[i, 2]:
+            if j in string.punctuation:
+                gmart.append(j)
+
+    if clean_df_g.iloc[i, 0].month == 4:
+        for j in clean_df_g.iloc[i, 2]:
+            if j in string.punctuation:
+                gapril.append(j)
+
+    if clean_df_g.iloc[i, 0].month == 5:
+        for j in clean_df_g.iloc[i, 2]:
+            if j in string.punctuation:
+                gmai.append(j)
+
+    if clean_df_g.iloc[i, 0].month == 6:
+        for j in clean_df_g.iloc[i, 2]:
+            if j in string.punctuation:
+                gjuni.append(j)
+
+
+# Turn month lists into string
+def tostring(s):
+    str1 = " "  # initialize an empty string
+    return str1.join(s)
+
+
+feb, mar, apr, may, jun = tostring(februari), tostring(mart), tostring(april), tostring(mai), tostring(juni)
+gfeb, gmar, gapr, gmay, gjun = tostring(gfebruari), tostring(gmart), tostring(gapril), tostring(gmai), tostring(gjuni)
+
+# print(gfeb, '\n', gmar, '\n', gapr, '\n', gmay, '\n', gjun)
+
+
+# View activity per month
+month = [2, 3, 4, 5, 6]
+
+for i in month:
+    g = date_df.loc[(date_df['Date'].dt.month == i) & (date_df['Name'] == "Guillermina")]
+    j = date_df.loc[(date_df['Date'].dt.month == i) & (date_df['Name'] == "Hardonk Jomme")]
+    g = g.groupby(pd.Grouper(key='Date', axis=0,
+                                        freq='T')).sum()
+    j = j.groupby(pd.Grouper(key='Date', axis=0,
+                             freq='T')).sum()
+    g = g.reset_index()
+    j = j.reset_index()
+
+    g.plot(x='Date', y='Characters')
+    # j.plot(x='Date', y='Characters')
+    plt.show()
 
